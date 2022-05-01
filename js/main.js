aaccce0863d540fb21e39c50e095a5f5528b2142
teramomo <104056227+teramomo@users.noreply.github.com>
2022-05-02 00:17:07 +0900

    5/2 update
    カルーセル付ける

--- js/main.js
+++ js/main.js
@@ -5,10 +5,6 @@
   const targets = document.querySelectorAll('.photo');
   console.log(targets);
 
-  //targetと交差したときの処理
-  // 第一引数　targetが交差したときの情報
-  // 第二引数　監視の状態の情報
-  // isIntersecting === true 発火している状態
   const callback = (entries, obs) => {
     entries.forEach(entry => {
       if(!entry.isIntersecting) {
@@ -23,17 +19,56 @@
     threshold: 0.5,
   };
 
-  
-  //第一引数：targetが監視領域を交差するたびに呼び出される処理（コールバック関数）
-  //第二引数：オプション設定。監視領域の指定、threshold(targetが領域に交差した％)
-  // ※コールバック関数は()なし。()を付けると実行された関数が呼び出されるので無限ループになる。
-
   const observer = new IntersectionObserver(callback, options);
 
-  //targetの監視
+  ////targetの監視
   targets.forEach(target => {
     observer.observe(target);
   });
+
+
+
+  //slider
+  const sliders = document.querySelector('.sliders');
+
+const carousel = document.querySelector('.carousel');
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+let direction = 0;
+
+prev.addEventListener('click', () => {
+  if(direction === -1) {
+    sliders.appendChild(sliders.firstElementChild);
+  }
+  direction = 1;
+  carousel.style.justifyContent = 'flex-end';
+  sliders.style.transform = 'translate(20%)';
+});
+
+
+next.addEventListener('click', () => {
+  direction = -1;
+  carousel.style.justifyContent = 'flex-start';
+  sliders.style.transform = 'translate(-20%)';
+});
+
+sliders.addEventListener('transitionend', () => {
+  if(direction === -1){
+    sliders.appendChild(sliders.firstElementChild);
+  }else if(direction === 1){
+    sliders.prepend(sliders.lastElementChild);
+  }
+
+  sliders.style.transition = 'none';
+  sliders.style.transform = 'translate(0)';
+
+  setTimeout( ()=> {
+    sliders.style.transition = 'all .5s';
+  });
+
+});
 
 
 
